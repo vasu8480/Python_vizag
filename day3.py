59219f2573b46e22cee84205b91f57377c527703
Vasu Nurukurthi <Vasu8480@gmail.com>
2026-03-23 15:03:23 +0530

    Refactor day3.py with comments and structure
    Refactor code for clarity and organization, adding comments to explain various programming concepts such as list unpacking, loops, and collections.

--- day3.py
+++ day3.py
@@ -1,154 +1,142 @@
-"""
-# a,b,c,d,*others=[1,2,3,4,5,54,64,5,6]
-# print(a,b,c,d,others)
+# ---------------- List Unpacking ----------------
+# a, b, c, d, *others = [1,2,3,4,5,54,64,5,6]
+# first 4 values go to a,b,c,d and rest go into 'others' as list
 
-a=list(map(int,input().split()))
+
+# ---------------- Input Handling ----------------
+a = list(map(int, input().split()))
+# takes space-separated input and converts to list of integers
 print(a)
 
-for i in range(0,50):
-	print(i)
 
-i=0
-while i<10:
-	print(i)
-	i+=1
-else:print("done")
+# ---------------- For Loop ----------------
+for i in range(0, 50):
+    print(i)   # prints 0 to 49
 
-continue  break  pass
-for i in range(1,11):
-	if i==5:
-		continue
-	print(i)
 
-for i in range(1,11):
-	if i==5:
-		break
-	print(i)
+# ---------------- While Loop ----------------
+i = 0
+while i < 10:
+    print(i)
+    i += 1
+else:
+    print("done")  # executes after loop completes
 
-for i in range(1,11):
-	if i==5:
-		pass
-#multplication table
-n=5
-for i in range(1,11):
-	print(n,'x',i,'=',n*i) 
 
-# 			*        
-#     * * *      
-#   * * * * *    
-# * * * * * * *  
-#       *        
-#       *
+# ---------------- Loop Control Statements ----------------
+# continue → skip current iteration
+# break → exit loop
+# pass → do nothing (placeholder)
 
-pic=[
-	[0,0,0,1,0,0,0],
-	[0,0,1,1,1,0,0],
-	[0,1,1,1,1,1,0],
-	[1,1,1,1,1,1,1],
-	[0,0,0,1,0,0,0],
-	[0,0,0,1,0,0,0]
+for i in range(1, 11):
+    if i == 5:
+        continue   # skip 5
+    print(i)
+
+for i in range(1, 11):
+    if i == 5:
+        break   # stop loop at 5
+    print(i)
+
+for i in range(1, 11):
+    if i == 5:
+        pass   # does nothing
+
+
+# ---------------- Multiplication Table ----------------
+n = 5
+for i in range(1, 11):
+    print(n, 'x', i, '=', n * i)
+
+
+# ---------------- Pattern using Matrix ----------------
+pic = [
+    [0,0,0,1,0,0,0],
+    [0,0,1,1,1,0,0],
+    [0,1,1,1,1,1,0],
+    [1,1,1,1,1,1,1],
+    [0,0,0,1,0,0,0],
+    [0,0,0,1,0,0,0]
 ]
 
 for i in pic:
-	for j in i:
-		if j==1:
-			print("*",end=" ")
-		else:
-			print(" ",end=" ")
-	print(" ")
-# #patterns
-'''
-*
-**   
-***  
-**** 
-*****
-'''
-for i in range(0,5):
-	for j in range(0,5):
-		print("*",end="")
-	print()
-
-    # *****
-    # *****
-    # *****
-    # *****
-    # *****
+    for j in i:
+        if j == 1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print(" ")   # move to next line
 
 
+# ---------------- Square Pattern ----------------
+for i in range(0, 5):
+    for j in range(0, 5):
+        print("*", end="")
+    print()   # new line after each row
 
+
+# ---------------- Triangle Pattern using List ----------------
 myList = []
-for i in range(1,5+1):
-    myList.append("*"*i)
-print(myList)
-print(type(myList))
-print("\n".join(myList))
-print(type("\n".join(myList)))
-print(myList)
 
-l1=[1,2,3,4,5,6,7,8,9,10]
-print(" ".join(list(map(str, l1))))
+for i in range(1, 6):
+    myList.append("*" * i)   # create pattern line
 
+print(myList)                # ['*', '**', '***', ...]
+print(type(myList))          # list
+
+print("\n".join(myList))     # print pattern line by line
+print(type("\n".join(myList)))  # string
 
 
-l1=["1","2","3","4","5"]
-#.join() method
-print(" ".join(str(l1)))
-print(" ".join(str(i) for i in l1))
+# ---------------- Join with Numbers ----------------
+l1 = [1,2,3,4,5,6,7,8,9,10]
+print(" ".join(list(map(str, l1))))  
+# convert int → str then join
 
 
-# 	  *
-#    **
-#   ***
-#  ****
- 
+l1 = ["1","2","3","4","5"]
+
+print(" ".join(str(l1)))  
+# wrong usage → joins characters of string representation
+
+print(" ".join(str(i) for i in l1))  
+# correct → joins elements
+
+
+# ---------------- Right-Aligned Triangle ----------------
 for row in range(1, 5):
-    print(" " * (5 - row-1) + "*" * row)
-		
+    print(" " * (5 - row - 1) + "*" * row)
 
 
-# # reverse for loop from 5 to 0
-
-# 1 1 1 1 1 
-# 2 2 2 2 
-# 3 3 3 
-# 4 4 
-# 5
-
-
-
+# ---------------- Number Pattern ----------------
 b = 5
+
 for i in range(6, 0, -1):
     b += 1
-    for j in range(0, i ):
+    for j in range(0, i):
         print(b, end=' ')
     print()
 
-# * * * * *
-# * * * *
-# * * *
-# * *
-# *
 
-
-
+# ---------------- Reverse Star Pattern ----------------
 for i in range(6, 0, -1):
     for j in range(0, i - 1):
         print("*", end=' ')
     print(" ")
 
 
-# sum oƒ  natural numbers
-# sum of digits
-# occurence of specific character
-
-
+# ---------------- Collections ----------------
 import collections
-a=dict(collections.Counter(['a','b','c','d','a','c']))
-print(a)
 
-dq=collections.deque([1,2,3,4,5,6,7])
-dq.apppend(100)
-dq.popleft()
+# Counter → count frequency of elements
+a = dict(collections.Counter(['a','b','c','d','a','c']))
+print(a)   # {'a':2, 'b':1, 'c':2, 'd':1}
+
+
+# Deque → fast append/pop from both ends
+dq = collections.deque([1,2,3,4,5,6,7])
+
+dq.append(100)   # add to right end
+dq.popleft()     # remove from left end
+
 print(dq)
-"""
